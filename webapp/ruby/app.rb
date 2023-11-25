c78e2ff255c8f3799833672fbc21f0dac133eb03
Ryota Egusa <egusa.ryota@gmail.com>
2023-11-25 13:40:51 +0900

    fix

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -1034,15 +1034,15 @@ module Isupipe
         # ランク算出
         users = tx.xquery('SELECT * FROM users').to_a
 
-        reactions_by_user_id = tx.xquery(<<~SQL).group_by { |row| row[:user_id] }.transform_values { |rows| rows.first }
-          SELECT u.id as user_id, COUNT(*) FROM users u
+        reactions_by_user_id = tx.xquery(<<~SQL).group_by { |row| row[:user_id] }.transform_values { |rows| rows.first[:cnt] }
+          SELECT u.id as user_id, COUNT(*) as cnt FROM users u
           INNER JOIN livestreams l ON l.user_id = u.id
           INNER JOIN reactions r ON r.livestream_id = l.id
           GROUP BY u.id
         SQL
         
-        tips_by_user_id = tx.xquery(<<~SQL).group_by { |row| row[:user_id] }.transform_values { |rows| rows.first }
-          SELECT u.id as user_id, IFNULL(SUM(l2.tip), 0) FROM users u
+        tips_by_user_id = tx.xquery(<<~SQL).group_by { |row| row[:user_id] }.transform_values { |rows| rows.first[:sm] }
+          SELECT u.id as user_id, IFNULL(SUM(l2.tip), 0) as sm FROM users u
             INNER JOIN livestreams l ON l.user_id = u.id
             INNER JOIN livecomments l2 ON l2.livestream_id = l.id
           GROUP BY u.id
