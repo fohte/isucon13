94bdf7c737526f0d13989170149fa3eac6059435
Ryota Egusa <egusa.ryota@gmail.com>
2023-11-25 17:55:23 +0900

    fix

--- webapp/sql/initdb.d/00_create_database.sql
+++ webapp/sql/initdb.d/00_create_database.sql
@@ -2,10 +2,10 @@ CREATE DATABASE IF NOT EXISTS `isupipe`;
 
 -- DROP USER IF EXISTS `isucon`@`%`;
 -- CREATE USER isucon IDENTIFIED BY 'isucon';
--- GRANT ALL PRIVILEGES ON isupipe.* TO 'isucon'@'%';
+GRANT ALL PRIVILEGES ON isupipe.* TO 'isucon'@'%';
 
 CREATE DATABASE IF NOT EXISTS `isudns`;
 
 -- DROP USER IF EXISTS `isudns`@`%`;
 -- CREATE USER isudns IDENTIFIED BY 'isudns';
--- GRANT ALL PRIVILEGES ON isudns.* TO 'isudns'@'%';
+GRANT ALL PRIVILEGES ON isudns.* TO 'isudns'@'%';
